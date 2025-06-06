b6cb373f166185e452a4657ad58a6e823e78f5f1
Daniil Klochkov <kldaniil@gmail.com>
2025-06-06 23:26:10 +0500

    removed test string

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,10 +2,9 @@ import random
 import string
 
 from .constants import SHORT_LINK_LENGTH
-
+    
 
 def generate_short_link(length=SHORT_LINK_LENGTH):
     return (
         ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     )
-def foo(): return  1
