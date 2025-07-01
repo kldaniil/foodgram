787f4ae4bac96cab2ebdc731f40f1d9cf5e38135
Daniil Klochkov <kldaniil@gmail.com>
2025-07-01 17:23:33 +0500

    попытка собрать фронт. Версия node 18. clearImmediate

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+global.clearImmediate = global.clearImmediate || ((id) => clearTimeout(id));
