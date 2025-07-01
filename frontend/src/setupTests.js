17aec5bcadf92d59ad72da87b6cf32f6d1cceb6a
Daniil Klochkov <kldaniil@gmail.com>
2025-07-01 17:29:07 +0500

    попытка собрать фронт 3

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,4 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-global.clearImmediate = global.clearImmediate || ((id) => clearTimeout(id));
+const timers = require('timers-browserify');
+global.clearImmediate = timers.clearImmediate;
+global.setImmediate = timers.setImmediate;
