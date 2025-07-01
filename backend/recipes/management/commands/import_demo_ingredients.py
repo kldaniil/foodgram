ecb93156aa5cf06a857da98b235713a53be0b1b9
Daniil Klochkov <kldaniil@gmail.com>
2025-07-01 20:19:09 +0500

    папки с ипортом и шрифтом внутри бэкенда

--- backend/recipes/management/commands/import_demo_ingredients.py
+++ backend/recipes/management/commands/import_demo_ingredients.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         '''Импортирует ингредиенты из файла ingredients.json'''
-        path = settings.BASE_DIR.parent / 'data' / 'ingredients.json'
+        path = settings.BASE_DIR / 'data' / 'ingredients.json'
         with open(path, encoding='utf-8') as file:
             ingredients = json.load(file)
             for ingredient in ingredients:
