bffdfc135551c0e100629f8e8c7e0f69b604a4f0
Daniil Klochkov <kldaniil@gmail.com>
2025-07-01 17:37:59 +0500

    reset frontend

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,6 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-const timers = require('timers-browserify');
-global.clearImmediate = timers.clearImmediate;
-global.setImmediate = timers.setImmediate;
